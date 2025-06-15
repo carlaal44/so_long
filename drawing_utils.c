3f6e68f0016815c5395f53925d07d9c57caff57c
Carla Fernandez Alvarez <carfern2@c1r12s4.42madrid.com>
2025-06-15 18:06:54 +0200

    update

--- drawing_utils.c
+++ drawing_utils.c
@@ -6,7 +6,7 @@
 /*   By: carfern2 <carfern2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 16:35:59 by carfern2          #+#    #+#             */
-/*   Updated: 2025/06/15 17:17:17 by carfern2         ###   ########.fr       */
+/*   Updated: 2025/06/15 18:04:10 by carfern2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,25 +23,19 @@ void	_draw_wall_tile(t_game *game, int x, int y)
 			x * TILE_SIZE, y * TILE_SIZE, 0xFFFFFF);
 }
 
-// Implementación de la función auxiliar para dibujar el tile del jugador
 void	_draw_player_tile(t_game *game, int x, int y)
 {
-	int	offset_x;
-	int	offset_y;
-
 	game->player_x = x;
 	game->player_y = y;
-	offset_x = (TILE_SIZE - 63) / 2; // Centrar el jugador horizontalmente
-	offset_y = TILE_SIZE - 63;       // Posicionar el jugador en la parte inferior
+	// No se necesitan offsets, ya que el muñeco es del mismo tamaño que la baldosa.
 	if (game->img_player)
 		mlx_put_image_to_window(game->mlx, game->win, game->img_player,
-			(x * TILE_SIZE) + offset_x, (y * TILE_SIZE) + offset_y);
+			x * TILE_SIZE, y * TILE_SIZE);
 	else
 		mlx_pixel_put(game->mlx, game->win,
 			x * TILE_SIZE, y * TILE_SIZE, 0xFF0000);
 }
 
-// Implementación de la función auxiliar para dibujar el tile de coleccionable
 void	_draw_collectible_tile(t_game *game, int x, int y)
 {
 	if (game->img_collectible)
@@ -49,10 +43,9 @@ void	_draw_collectible_tile(t_game *game, int x, int y)
 			x * TILE_SIZE, y * TILE_SIZE);
 	else
 		mlx_pixel_put(game->mlx, game->win,
-			x * TILE_SIZE, y * TILE_SIZE, 0x00FF00);
+			x * TILE_SIZE, y * TILE_SIZE, 0x00FF00); // Fixed typo: TILF_SIZE to TILE_SIZE
 }
 
-// Implementación de la función auxiliar para dibujar el tile de salida
 void	_draw_exit_tile(t_game *game, int x, int y)
 {
 	if (game->img_exit)
@@ -63,7 +56,6 @@ void	_draw_exit_tile(t_game *game, int x, int y)
 			x * TILE_SIZE, y * TILE_SIZE, 0x0000FF);
 }
 
-// Esta función principal delega el dibujo específico a las auxiliares
 void	_draw_specific_tile(t_game *game, int x, int y, char tile_type)
 {
 	if (tile_type == '1')
@@ -75,4 +67,3 @@ void	_draw_specific_tile(t_game *game, int x, int y, char tile_type)
 	else if (tile_type == 'E')
 		_draw_exit_tile(game, x, y);
 }
-
